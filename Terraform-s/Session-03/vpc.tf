5930bcd6d800da0e616d1482d55c4717314a13a5
chakradhar <chakradhar.thatikonda933@gmail.com>
2023-05-10 16:32:57 +0530

    vpc

--- Terraform-s/Session-03/vpc.tf
+++ Terraform-s/Session-03/vpc.tf
@@ -1,34 +1,32 @@
-resource "aws_vpc" "edstem-vpc" { #this name belongs to only terraform reference
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id # it will fetch VPC ID created from above code
+  cidr_block = var.public_subnet_cidr # this is hard coding
+
+  tags = var.tags 
+}
+
+resource "aws_vpc" "main" { #this name belongs to only terraform reference
 
     cidr_block       = var.cidr
     instance_tenancy = "default"
-    tags = {
-        Name = "automated-vpc" #this name belongs to AWS
-    }
-}
-
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.edstem-vpc.id # it will fetch VPC ID created from above code
-  cidr_block = var.cidr
-
-  tags = var.tags
+    tags = var.tags
 }
 
 resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.edstem-vpc.id # it will fetch VPC ID created from above code
+  vpc_id     = aws_vpc.main.id # it will fetch VPC ID created from above code
   cidr_block = "10.0.2.0/24"
 
   tags = var.tags
 }
 
 resource "aws_internet_gateway" "automated-igw" {
-  vpc_id = aws_vpc.edstem-vpc.id # internet gateway depends on VPC
+  vpc_id = aws_vpc.main.id # internet gateway depends on VPC
 
-  tags = var.tags
+  tags =  var.tags
 }
 
 resource "aws_route_table" "public-rt" {
-  vpc_id = aws_vpc.edstem-vpc.id
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -42,23 +40,23 @@ resource "aws_eip" "auto-eip" {
 
 }
 
-resource "aws_nat_gateway" "nat" {
+resource "aws_nat_gateway" "example" {
   allocation_id = aws_eip.auto-eip.id
   subnet_id     = aws_subnet.public.id
 
   tags = var.tags
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
-   #on the Internet Gateway for the VPC.
-  #depends_on = [aws_internet_gateway.automated-igw]
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.automated-igw]
 }
 
 resource "aws_route_table" "private-rt" { #for private route we don't attach IGW, we attach NAT
-  vpc_id = aws_vpc.edstem-vpc.id
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat.id
+    gateway_id = aws_nat_gateway.example.id
   }
 
   tags = var.tags
