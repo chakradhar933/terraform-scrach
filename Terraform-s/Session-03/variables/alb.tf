d6b9c91e5b2bc623571c56dc00069e530f169a4d
chakradhar <chakradhar.thatikonda933@gmail.com>
2023-05-10 18:12:30 +0530

    alb

--- Terraform-s/Session-03/variables/alb.tf
+++ Terraform-s/Session-03/variables/alb.tf
@@ -2,16 +2,10 @@ resource "aws_lb" "ALB" {
   name               = "edstem"
   internal           = var.internet
   load_balancer_type = var.lb_type
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
+  #security_groups    = [aws_security_group.lb_sg.id]
+  #subnets            = [for subnet in aws_subnet.public : subnet.id]
 
   enable_deletion_protection = true
-
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.id
-    prefix  = "test-lb"
-    enabled = true
-  }
 
   tags = {
     Environment = "production"
