ec9ff3844ef63bc02a3ef239dfd918bc158ef217
chakradhar <chakradhar.thatikonda933@gmail.com>
2023-05-10 12:41:43 +0530

    remote-state

---
+++ Terraform-s/Session-03/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "s3" {
+    bucket = "edstem-project933"
+    region = "us-east-1"
+    dynamodb_table = "edstem-project"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
