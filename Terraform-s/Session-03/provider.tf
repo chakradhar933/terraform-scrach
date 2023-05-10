4a1ce9b271c8bd8371ee8843b3778bdde4e74bbf
chakradhar <chakradhar.thatikonda933@gmail.com>
2023-05-10 13:06:20 +0530

    provider

--- Terraform-s/Session-03/provider.tf
+++ Terraform-s/Session-03/provider.tf
@@ -1,18 +1,21 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+      source = "hashicorp/aws"
+      version = "4.64.0"
     }
   }
+
   backend "s3" {
     bucket = "edstem-project933"
+    key    = "project"
     region = "us-east-1"
     dynamodb_table = "edstem-lock"
   }
 }
 
-## Configure the AWS Provider
 provider "aws" {
+  # Configuration options
   region = "us-east-1"
+  
 }
