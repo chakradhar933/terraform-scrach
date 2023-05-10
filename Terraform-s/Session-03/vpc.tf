7df58180ad83f057617be198467919badbc2fd8b
chakradhar <chakradhar.thatikonda933@gmail.com>
2023-05-10 15:09:13 +0530

    vpc

--- Terraform-s/Session-03/vpc.tf
+++ Terraform-s/Session-03/vpc.tf
@@ -82,7 +82,7 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public-rt.id
 }
 
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private-rt.id
-}
+#resource "aws_route_table_association" "private" {
+ # subnet_id      = aws_subnet.private.id
+  #route_table_id = aws_route_table.private-rt.id
+#}
