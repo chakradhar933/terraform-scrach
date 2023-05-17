7c4c6e62251418d226e1c83423c8568dc0cb9912
chakradhar <chakradhar.thatikonda933@gmail.com>
2023-05-17 15:41:52 +0530

    count

--- Terraform-s/Session-05/ec2.tf
+++ Terraform-s/Session-05/ec2.tf
@@ -1,7 +1,7 @@
 # our requirement is to create 3 instances name it as web-server,app-server,DB-server.
 resource "aws_instance" "web"{
 
-    ami = "ami-046b3d63c4fe1b3cf" #AMI ID will be different every region, this is OS
+    ami = "ami-06a0cd9728546d178" #AMI ID will be different every region, this is OS
     instance_type = "t3.micro"
     count = 3
     tags = {
