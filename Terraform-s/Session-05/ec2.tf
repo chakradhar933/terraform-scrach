9a6120d41573d638c5230e42fbb4d168583957be
chakradhar <chakradhar.thatikonda933@gmail.com>
2023-05-17 15:24:37 +0530

    count

--- Terraform-s/Session-05/ec2.tf
+++ Terraform-s/Session-05/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web"{
     ami = "ami-046b3d63c4fe1b3cf" #AMI ID will be different every region, this is OS
     instance_type = "t3.micro"
     count = 3
-}
-tags {
-    name = "web"
+    tags {
+        name = "web"
+    }
 }
