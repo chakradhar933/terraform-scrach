ddfce0a80ad30efb37438eb30318bc80477e4068
chakradhar <chakradhar.thatikonda933@gmail.com>
2023-05-17 12:50:03 +0530

    ec2

--- Terraform-s/Session-04/ec2.tf
+++ Terraform-s/Session-04/ec2.tf
@@ -2,4 +2,10 @@ resource "aws_instance" "Timing" {
 
     ami = data.aws_ami.ami_info.image_id #AMI ID will be different every region, this is OS
     instance_type = "t3.micro"
+    key_name = aws_key_pair.Terraform.key_name
+}
+resource "aws_key_pair" "Terraform" {
+  key_name   = "Terraform"
+  # This file function can read the terraform123.pub and then fetch the content.
+  public_key = file("${path.module}/terraform123.pub")
 }
