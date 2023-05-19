0c41d7d8dd47355bab97611eb26e30c36e474516
chakradhar <chakradhar.thatikonda933@gmail.com>
2023-05-19 11:29:05 +0530

    data

--- Terraform-s/practice/ec2.tf
+++ Terraform-s/practice/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "servers" {
     ami = data.aws_ami.ami-info.image_id
     instance_type = var.instance_type
-    count = 1
+    #count = 1
     tags = {
         Name = "web"
     }
